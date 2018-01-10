802a4bba7a9213c67ea784f5bb3e169749ce0ee3
AravindSama <aravind.sama12@gmail.com>
2018-01-10 13:01:59 -0600

    Spring Boot with SOAP web services

---
+++ src/main/java/com/aravind/endpoint/WebServiceEndPoint.java
@@ -0,0 +1,30 @@
+package com.aravind.endpoint;
+
+
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import com.aravind.InputSOATest;
+import com.aravind.ObjectFactory;
+import com.aravind.OutputSOATest;
+
+@Endpoint
+public class WebServiceEndPoint {
+
+	private static final String NAMESPACE_URI = "http://javainuse.com";
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "inputSOATest")
+	@ResponsePayload
+	public OutputSOATest hello(@RequestPayload InputSOATest request) {
+
+		String outputString = "Hello " + request.getTest() + "!";
+
+		ObjectFactory factory = new ObjectFactory();
+		OutputSOATest response = factory.createOutputSOATest();
+		response.setResult(outputString);
+
+		return response;
+	}
+}
